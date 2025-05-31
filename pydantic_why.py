1641815bf6b48c4e35d3ea9f1c33d3675fbb1a75
Muhammad Asif <programmerasif@outlook.com>
2025-05-31 11:03:39 +0500

    added complex types

--- pydantic_why.py
+++ pydantic_why.py
@@ -1,22 +1,42 @@
 
 from pydantic import BaseModel
+from typing import List, Dict
 
 class Patient(BaseModel):
     name: str
     age: int
+    wieght: float
+    married: bool
+    alergies: List[str]
+    contact_details: Dict[str, str]
+
+
 
 def insert_patient_data(patient:Patient):
     print(patient.name)
     print(patient.age)
+    print(patient.wieght)
+    print(patient.married)
+    print(patient.alergies)
+    print(patient.contact_details)
+    
+
     print("inserted successfully.")
 
 def update_patient_data(patient:Patient):
     print(patient.name)
     print(patient.age)
-    print("Updated successfully.")
+    print(patient.wieght)
+    print(patient.married)
+    print(patient.alergies)
+    print(patient.contact_details)
 
 
-patient_info = {"name": "Asif","age": 30}
+patient_info = {"name": "Asif","age": 30, "wieght": 70.5, "married": True,
+                "alergies": ["pollen", "dust"],
+                "contact_details": {"email": "abc@gmail.com", "phone": "1234567890"}}
+
+
 patient_data = Patient(**patient_info)    
 insert_patient_data(patient_data)
 update_patient_data(patient_data)
