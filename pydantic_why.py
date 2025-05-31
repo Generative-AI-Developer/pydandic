36d81584a111ee64d3d3995b377b38aa60122017
Muhammad Asif <programmerasif@outlook.com>
2025-05-31 09:38:12 +0500

    pydantic added

--- pydantic_why.py
+++ pydantic_why.py
@@ -1,5 +1,24 @@
-def inser_patient_data(name, age):
-    print(name)
-    print(age)
-    print("Patient data inserted successfully.")
-inser_patient_data("asif", "thirty")
+
+from pydantic import BaseModel
+
+class Patient(BaseModel):
+    name: str
+    age: int
+
+def insert_patient_data(patient:Patient):
+    print(patient.name)
+    print(patient.age)
+    print("inserted successfully.")
+
+def update_patient_data(patient:Patient):
+    print(patient.name)
+    print(patient.age)
+    print("Updated successfully.")
+
+
+patient_info = {"name": "Asif","age": 30}
+patient_data = Patient(**patient_info)    
+insert_patient_data(patient_data)
+update_patient_data(patient_data)
+
+
